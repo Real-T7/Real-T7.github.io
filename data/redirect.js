35313736c9a555a12998a7cde2ff5dbb023d34ad
T7 <tigervn0709@gmail.com>
2025-06-05 15:43:34 +0700

    Update

--- data/redirect.js
+++ data/redirect.js
@@ -2,17 +2,6 @@ const otherRadio = document.getElementById("otherRadio");
 const otherInput = document.getElementById("otherInput");
 const subdirRadios = document.getElementsByName("subdir");
 
-for (const radio of subdirRadios) {
-  radio.addEventListener("change", () => {
-    if (otherRadio.checked) {
-      otherInput.classList.add("active");
-      otherInput.focus();
-    } else {
-      otherInput.classList.remove("active");
-    }
-  });
-}
-
 function redirectToKeyword() {
   const keyword = document.getElementById("keywordInput").value.trim();
   if (!keyword) return;
