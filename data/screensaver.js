b89f4cebfde985dd192a80615e249e7f5401889a
T7 <tigervn0709@gmail.com>
2025-06-06 21:27:04 +0700

    Update screensaver.js

--- data/screensaver.js
+++ data/screensaver.js
@@ -1,19 +1,19 @@
-      let timeout;
+let timeout;
 
-      function resetTimer() {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          window.location.href = "/html/games/screensaver/";
-        }, 300000);
-      }
+function resetTimer() {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    window.location.href = "/html/games/screensaver/";
+  }, 300000);
+}
 
-      function setupInactivityTimer() {
-        window.addEventListener("mousemove", resetTimer);
-        window.addEventListener("keydown", resetTimer);
-        window.addEventListener("scroll", resetTimer);
-        window.addEventListener("touchstart", resetTimer);
+function setupInactivityTimer() {
+  window.addEventListener("mousemove", resetTimer);
+  window.addEventListener("keydown", resetTimer);
+  window.addEventListener("scroll", resetTimer);
+  window.addEventListener("touchstart", resetTimer);
 
-        resetTimer();
-      }
+  resetTimer();
+}
 
-      window.onload = setupInactivityTimer;
+window.onload = setupInactivityTimer;
