f6432e374128b66b77967cf20379142561a1e969
T7 <tigervn0709@gmail.com>
2025-06-06 23:19:45 +0700

    Update index.html
    i finally decided to revert all these changes bc none of them are working

--- data/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCiIQr2VpfkBgKNiJwiSQW8ivOSZ9t0lnw",
-  authDomain: "t7-github.firebaseapp.com",
-  databaseURL: "https://t7-github-default-rtdb.firebaseio.com",
-  projectId: "t7-github",
-  storageBucket: "t7-github.firebasestorage.app",
-  messagingSenderId: "560364753472",
-  appId: "1:560364753472:web:4d274d57c93643764785a5"
-};
