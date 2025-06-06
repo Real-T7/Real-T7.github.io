c10c59d66d55d0315e6a8847d58c77e1a56221de
T7 <tigervn0709@gmail.com>
2025-06-06 13:06:41 +0700

    Update redirect.js

--- data/redirect.js
+++ data/redirect.js
@@ -21,7 +21,7 @@ function redirectToKeyword() {
   const match = idMap.find(entry => entry.ids.includes(keyword));
 
   if (match) {
-    window.location.href = window.location.origin + '/' + match.path;
+    window.location.href = `/${match.path}`;
   } else {
     window.location.href = `/${keyword}`
   }
