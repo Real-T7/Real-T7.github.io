00662bc6b50336ac43a53c2fe59ff7066b1ce61b
T7 <tigervn0709@gmail.com>
2025-06-07 16:32:29 +0700

    Revert "Update firebase-init.js"
    This reverts commit 9dbfdbe20661f3750946f28ef91e5e4a0e3dfc4f.

--- data/firebase-init.js
+++ data/firebase-init.js
@@ -29,6 +29,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.database();
 console.log("Firebase initialized.");
+
+document.dispatchEvent(new Event('firebase-ready'));
 };
 
 // Run it
