179a1ae79656dca95ec19e27aca1f502a89b54aa
T7 <tigervn0709@gmail.com>
2025-06-21 14:33:46 +0700

    Visual Changes v2
    Visual Changes v100 will be very soon

--- assets/data/404-clicker.js
+++ assets/data/404-clicker.js
@@ -3,10 +3,27 @@ let clicks = 0;
 
 const clicker = document.getElementById("404");
 
+const colors = [
+  '#26de81', '#fc5c65', '#fd9644', '#fed330',
+  '#2bcbba', '#45aaf2', '#4b7bec', '#a55eea',
+  '#ffc1f3', '#76ead7', '#ff9c71', '#32e0c4',
+  '#d291bc', '#fa744f'
+];
+
+let previousColor = null
+
 function handleClick() {
   if (clicks >= target) return;
 
   clicks++;
+
+  do {
+    randomColor = colors[Math.floor(Math.random() * colors.length)];
+  } while (randomColor === previousColor);
+
+  previousColor = randomColor;
+
+  clicker.style.color = randomColor;
 
   if (clicks === target) {
     console.log(`Page Found! ( x${clicks} )`);
