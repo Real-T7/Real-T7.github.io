047d08c3dc1cd1ddcdd83f34646f8b6d92f5266b
T7 <tigervn0709@gmail.com>
2025-06-21 12:28:41 +0700

    The 404 Update v2
    yes 😔

--- assets/data/404-clicker.js
+++ assets/data/404-clicker.js
@@ -3,16 +3,20 @@ let clicks = 0;
 
 const clicker = document.getElementById("404");
 
-clicker.addEventListener("click", () => {
-    clicks++;
+function handleClick() {
+  clicks++;
 
-    if (clicks === target) {
-        console.log(`Page Found! ( x${clicks} )`)
+  if (clicks === target) {
+      console.log(`Page Found! ( x${clicks} )`);
 
-        setTimeout(() => {
-            window.location.href = "//deltarune.com/lancer/";
-        }, 1000);
-    } else {
-        console.log(`Page Not Found ( x${clicks} )`)
-    }
-});
+      clicker.removeEventListener("click", handleClick);
+
+      setTimeout(() => {
+        window.location.href = "//deltarune.com/lancer/";
+      }, 1000);
+  } else {
+    console.log(`Page Not Found ( x${clicks} )`);
+  }
+}
+
+clicker.addEventListener("click", handleClick);
