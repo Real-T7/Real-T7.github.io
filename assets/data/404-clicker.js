68429b2521099cf5395b604c9e6755ae7c972bac
T7 <tigervn0709@gmail.com>
2025-06-21 12:32:13 +0700

    The 404 Update v3
    🙏😭💔😔

--- assets/data/404-clicker.js
+++ assets/data/404-clicker.js
@@ -4,18 +4,20 @@ let clicks = 0;
 const clicker = document.getElementById("404");
 
 function handleClick() {
+  if (clicks >= target) return;
+
   clicks++;
 
   if (clicks === target) {
-      console.log(`Page Found! ( x${clicks} )`);
+    console.log(`Page Found! ( x${clicks} )`);
 
-      clicker.removeEventListener("click", handleClick);
+    clicker.removeEventListener("click", handleClick);
 
-      setTimeout(() => {
-        window.location.href = "//deltarune.com/lancer/";
-      }, 1000);
+    setTimeout(() => {
+      window.location.href = "//deltarune.com/lancer/";
+    }, 1000);
   } else {
-    console.log(`Page Not Found ( x${clicks} )`);
+      console.log(`Page Not Found ( x${clicks} )`);
   }
 }
 
