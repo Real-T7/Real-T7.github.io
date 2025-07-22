afed27c85ecdcd7d8c5ae9ebbe1978de97f0a11d
T7 <tigervn0709@gmail.com>
2025-07-22 13:57:24 +0700

    looping bug fix
    fixed bug where it keeps looping you back to the warning page
    + fixed and improved the proceed function

--- assets/data/mobile.js
+++ assets/data/mobile.js
@@ -1,8 +1,10 @@
 (function() {
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
   const hasOptedOut = localStorage.getItem('hideMobileWarning') === 'true';
+  const hasSessionOptOut = sessionStorage.getItem('skipMobileWarning') === 'true';
+  const isWarningPage = window.location.pathname === '/mobile.html';
 
-  if (isMobile && !hasOptedOut) {
+  if (isMobile && !hasOptedOut && !hasSessionOptOut && !isWarningPage) {
     sessionStorage.setItem('previousUrl', window.location.href);
     window.location.href = '/mobile.html';
   }
