d4d60d98fe587c64c592317d1988b34892987039
Real-T7 <tigervn0709@gmail.com>
2025-08-20 10:23:40 +0700

    Minor Bugfix (v333)
    - some bugfixes (duh)
    - reworked the visit system thingy
    - music player's position is now absolute in that one page

--- assets/data/account.js
+++ assets/data/account.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("DOMContentLoaded", function() {
   const bannedNames = ["T7", "S-762", "S-575"];
   const username = localStorage.getItem("account-username");
 
@@ -6,10 +6,27 @@ window.addEventListener('DOMContentLoaded', function() {
     localStorage.removeItem("account-username");
   }
 
-  const usernameToDisplay = localStorage.getItem("account-username");
-  const usernameElements = document.querySelectorAll(".current-username");
+  const usernameToDisplay = localStorage.getItem("account-username") || "stranger";
 
-  usernameElements.forEach(element => {
-    element.textContent = usernameToDisplay ? usernameToDisplay : "stranger";
-  });
+  const path = window.location.pathname;
+  const isMainPage = (path === "/" || path.endsWith("/index.html"));
+
+  if (isMainPage) {
+    const hasVisited = localStorage.getItem("visited");
+    const div = document.querySelector(".current-account");
+
+    if (div) {
+      if (!hasVisited) {
+        div.innerHTML = `welcome, <span class="current-username">${usernameToDisplay}</span>!`;
+        localStorage.setItem("visited", "true");
+      } else {
+        div.innerHTML = `welcome back, <span class="current-username">${usernameToDisplay}</span>!`;
+      }
+    }
+  } else {
+    const usernameElements = document.querySelectorAll(".current-username");
+    usernameElements.forEach(element => {
+      element.textContent = usernameToDisplay;
+    });
+  }
 });
